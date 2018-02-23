b5f175e9c5ceb04005868b036e0559c8a504b720
Dragota Mircea <dragotamircea@Dragotas-iMac.local>
2018-02-23 15:07:09 +0200

    initial commit

--- EmojiArt/EmojiArtView.swift
+++ EmojiArt/EmojiArtView.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class EmojiArtView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    var backgroundImage:UIImage? {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
     override func draw(_ rect: CGRect) {
         // Drawing code
+        backgroundImage?.draw(in: bounds)
     }
-    */
+
 
 }
