9007d7d693559e965b36b55f415b7afc4dc08792
Dragota Mircea <dragotamircea@Dragotas-iMac.local>
2018-02-23 15:07:00 +0200

    initial commit

---
+++ EmojiArt/EmojiArtView.swift
@@ -0,0 +1,21 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by Dragota Mircea on 23/02/2018.
+//  Copyright © 2018 Dragota Mircea. All rights reserved.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
